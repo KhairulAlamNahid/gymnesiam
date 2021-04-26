f425904324bbcb0cf24201b214ee4674ab463ccb
KhairulAlamNahid <profreelancer10@gmail.com>
2021-04-26 15:29:52 +0600

    upload file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// banner slider js
+$('#banner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    infinite: true,
+    arrows:false,
+    dots:true,
+    autoplaySpeed: 3000,
+});
+
+// video venobox js
+$('.venobox').venobox();
